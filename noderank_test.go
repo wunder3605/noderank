56570e7605b08f82d6740d5c87fdc6d2f1757427
jdyuankai <yuank1987@163.com>
2019-02-28 20:55:19 +0800

    add noderank  test

--- noderank_test.go
+++ noderank_test.go
@@ -1,26 +1,25 @@
 package noderank
 
 import (
+	"github.com/wunder3605/noderank"
 	"reflect"
 	"testing"
 )
 
 func TestGetRank(t *testing.T) {
 
-	var cli = new(CLI)
-
 	info := []string{"A", "B", "1"}
-	cli.addAttestationInfo(info)
+	noderank.AddAttestationInfo(info)
 	info = []string{"B", "C", "1"}
-	cli.addAttestationInfo(info)
+	noderank.AddAttestationInfo(info)
 	info = []string{"C", "D", "1"}
-	cli.addAttestationInfo(info)
+	noderank.AddAttestationInfo(info)
 	info = []string{"D", "A", "1"}
-	cli.addAttestationInfo(info)
+	noderank.AddAttestationInfo(info)
 	info = []string{"A", "C", "1"}
-	cli.addAttestationInfo(info)
+	noderank.AddAttestationInfo(info)
 
-	actural := cli.getRank("1", 1)
+	actural := noderank.GetRank("1", 1)
 
 	if reflect.DeepEqual(len(actural), 1) != true {
 		t.Error("expected", 1, "but got ", len(actural))
